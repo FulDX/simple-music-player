2e2e3eb7d8c6f50b3f64ef76fa487f557f7f663d
Ping121 <lucadar76@gmail.com>
2026-01-31 00:01:34 +0700

    Ngebug sitik

--- src/script.js
+++ src/script.js
@@ -1,10 +1,78 @@
+// ID VAR
 let slider = document.getElementById("slider");
 let song = document.getElementById("song");
 let ctrlIcon = document.querySelector("#ctrlIcon i");
 let currentTime = document.getElementById("current-time");
 let duration = document.getElementById("duration");
+let shuffle = document.getElementById("shuffle");
 let repeat = document.getElementById("repeat");
+let title = document.getElementById("title");
+let artist = document.getElementById("artist");
+let img = document.getElementById("image");
+
 let isRepeat = false;
+let isShuffle = false;
+let indexOfSong = 0;
+
+// SONGS DATA
+const songs = [
+  {
+    title: "時の傷痕 ハジマリノ 鼓動",
+    artist: "Yasunori Mitsuda",
+    src: "/media/mp3/ndak tawu.mp3",
+    img: "/media/img/1.jpg",
+  },
+  {
+    title: "Mimpi Buruk",
+    artist: "Lil Salmonella",
+    src: "/media/mp3/Lil Salmonela - Mimpi Buruk.mp3",
+    img: "/media/img/mimpiburuk.jpg",
+  },
+  {
+    title: "Odorouze",
+    artist: "Yorushika",
+    src: "/media/mp3/odorouze.mp3",
+    img: "/media/img/odorouze.jpg",
+  },
+  {
+    title: "Nandemonaiya",
+    artist: "RADWIMPS",
+    src: "/media/mp3/Nandemonaiya.mp3",
+    img: "/media/img/radwimps.jpg",
+  },
+];
+
+title.textContent = songs[0].title;
+artist.textContent = songs[0].artist;
+song.src = songs[0].src;
+img.src = songs[0].img;
+
+function prevSong() {
+  indexOfSong--;
+  if (indexOfSong < 0) {
+    indexOfSong = songs.length - 1;
+  }
+  console.log(indexOfSong);
+  changeSong(indexOfSong);
+  playSong();
+}
+
+function nextSong() {
+  indexOfSong++;
+  if (indexOfSong >= songs.length) {
+    indexOfSong = 0;
+  }
+  console.log(indexOfSong);
+  changeSong(indexOfSong);
+  playSong();
+}
+
+function changeSong(index) {
+  title.textContent = songs[index].title;
+  artist.textContent = songs[index].artist;
+  song.src = songs[index].src;
+  img.src = songs[index].img;
+}
 
 function formatTime(time) {
   let minutes = Math.floor(time / 60);
@@ -26,40 +94,65 @@ song.onloadedmetadata = function () {
 song.ontimeupdate = function () {
   slider.value = song.currentTime;
   currentTime.textContent = formatTime(song.currentTime);
-  if (song.currentTime == song.duration) {
-    if (isRepeat) {
-      song.currentTime = 0;
-      song.play();
-    } else {
-      playPause();
-    }
+};
+
+song.onended = function () {
+  if (isRepeat) {
+    song.currentTime = 0;
+    playSong();
+  } else if (isShuffle) {
+    let random;
+    do {
+      random = Math.floor(Math.random() * songs.length);
+    } while (random === indexOfSong);
+
+    indexOfSong = random;
+    changeSong(indexOfSong);
+    playSong();
+  } else {
+    nextSong();
   }
 };
 
-if (song.play()) {
-  setInterval(() => {
-    slider.value = song.currentTime;
-  }, 500);
+function playSong() {
+  song.play();
+  ctrlIcon.classList.add("fa-pause");
+  ctrlIcon.classList.remove("fa-play");
 }
 
-const repeatFunc = () => {
-  if (repeat.classList.contains("text-green-400")) {
+function pauseSong() {
+  song.pause();
+  ctrlIcon.classList.remove("fa-pause");
+  ctrlIcon.classList.add("fa-play");
+}
+
+function togglePlay() {
+  if (song.paused) {
+    playSong();
+  } else {
+    pauseSong();
+  }
+}
+
+function repeatFunc() {
+  isRepeat = !isRepeat;
+
+  if (isRepeat) {
+    repeat.classList.add("text-green-400");
+    isShuffle = false;
+    shuffle.classList.remove("text-green-400");
+  } else {
+    repeat.classList.remove("text-green-400");
+  }
+}
+
+function shuffleFunc() {
+  isShuffle = !isShuffle;
+  if (isShuffle) {
+    shuffle.classList.add("text-green-400");
     isRepeat = false;
     repeat.classList.remove("text-green-400");
   } else {
-    isRepeat = true;
-    repeat.classList.add("text-green-400");
+    shuffle.classList.remove("text-green-400");
   }
-};
-
-const playPause = () => {
-  if (ctrlIcon.classList.contains("fa-pause")) {
-    song.pause();
-    ctrlIcon.classList.remove("fa-pause");
-    ctrlIcon.classList.add("fa-play");
-  } else {
-    song.play();
-    ctrlIcon.classList.add("fa-pause");
-    ctrlIcon.classList.remove("fa-play");
-  }
-};
+}
